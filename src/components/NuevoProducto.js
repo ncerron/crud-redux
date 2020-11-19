4dfd6945d8b0dc6c2741b79b7a255e14baf58eba
ncerron <natalia.cerron@gmail.com>
2020-11-19 14:45:44 -0300

    prueba

---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,41 @@
+import React from "react";
+const NuevoProducto = () => {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+            <form>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control mb-3"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                ></input>
+
+                <input
+                  type="number"
+                  className="form-control mb-3"
+                  placeholder="Precio Producto"
+                  name="precio"
+                ></input>
+                <button
+                  type="submit"
+                  className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                >
+                  Agregar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NuevoProducto;
